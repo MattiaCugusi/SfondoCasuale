c7c6a1cac8ef7c8eff47360795b7bc1c66b88662
MattiaCugusi <m.cugusi287@gmail.com>
2024-10-04 09:37:43 +0200

    esercizio completato

--- SfondoCasuale.php
+++ SfondoCasuale.php
@@ -6,6 +6,27 @@
     <title>Sfondo Casuale</title>
 </head>
 <body>
+    <?php
+     function sfondoCasuale($lar){
+        if ($lar <150){
+            return "#7FFFD4";
+        }else if ($lar >=150 && $lar <= 250){
+            return "#1E90FF";
+        }else{
+            return "#0000FF";
+        }
+    }
+
+    $larghezza = rand(50,500);
+
+    $colore = sfondoCasuale($larghezza);
+
+    echo "<div style='width:" . $larghezza . "px; height:" . ($larghezza*2) . "px; background-color:" . $colore . "'></div>"
+
+
+
+
+    ?>
     
 </body>
 </html>
